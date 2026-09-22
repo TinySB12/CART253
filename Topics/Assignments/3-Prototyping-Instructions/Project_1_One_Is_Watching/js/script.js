85bf353aec0dfbed734e53ee62b0e7afed20af35
TinySB12 <saba.b.moghaddam@gmail.com>
2026-09-22 19:19:56 -0400

    Drew most of the body for the first project

--- Topics/Assignments/3-Prototyping-Instructions/Project_1_One_Is_Watching/js/script.js
+++ Topics/Assignments/3-Prototyping-Instructions/Project_1_One_Is_Watching/js/script.js
@@ -23,6 +23,49 @@ function setup() {
 */
 function draw() {
     background(197, 217, 222);
+
+
+    //her legs :)
+    fill(123,151,246);
+    rect(350, 550, 80, 100);
+
+    fill(123,151,246);
+    rect(475, 550, 80, 100);
+
+    //Her body but unfortunately I didn't know how to do the head shape proper like the reference
+    fill(90, 124, 240);
+    strokeWeight(5);
+    rect(360, 150, 180, 400, 20);
+
+    fill(90, 124, 240);
+    strokeWeight(5);
+    rect(255, 400, 400, 150, 30);
+
+    fill(90, 124, 240);
+    strokeWeight(5);
+    rect(300, 150, 176, 150, 20);
+
+    noStroke();
+    fill(90, 124, 240);
+    rect(362, 152, 176, 300, 20);
+    stroke(0);
+    
+    //Her eyes and mouth
+    fill(255, 255, 255);
+    ellipse(380, 300, 100, 110);
+
+    fill(255, 255, 255);
+    ellipse(520, 300, 100, 110);
+
+    noFill();
+    arc(450, 400, 90, 130, 0, PI);
+    
      
+    
+    
+
+
+
+
 
 }
