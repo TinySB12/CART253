c4127cf78c14da107545f0f3f677a464168fa254
TinySB12 <saba.b.moghaddam@gmail.com>
2026-09-23 19:57:19 -0400

    Began the variables challenge. Did Step 1, 2 and 3

--- Topics/Assignments/4-Variables-Instructions/js/script.js
+++ Topics/Assignments/4-Variables-Instructions/js/script.js
@@ -1,73 +1,64 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Mr. Furious
+ * Pippin Barr
+ *
+ * A guy who becomes visibly furious!
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
-    createCanvas(700, 500)
-
-}
-
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-
-let rect_size = 10;
-let ellipse_size = 50;
-
-let bird = {
-    birdX:50,
-    birdY:90,
-    birdSize:50,
-    r:0,
-    g:0,
-    b:255
-
-}
-
-let sun = {
-    sunX: 20,
-    sunY: 50,
-    sunsize: 10,
+// Our friend Mr. Furious
+let mrFurious = {
+  // Position and size
+  x: 200,
+  y: 200,
+  size: 100,
+  // Colour
+  fill: {
     r: 255,
-    g: 255,
-    b: 0
+    g: 225,
+    b: 225
+  }
+};
+
+let Sky = {
+    r:160,
+    g:180,
+    b:200
+
 }
+
+/**
+ * Create the canvas
+ */
+function setup() {
+  createCanvas(400, 400);
+}
+
+/**
+ * Draw (and update) Mr. Furious
+ */
 function draw() {
-    background(0,0,0)
-    noStroke()
-    fill(255,0,0)
-    fill(sun.r,sun.g,sun.b)
-    ellipse(sun.sunX,sun.sunY,sun.sunsize,sun.sunsize)
+  background(Sky.r, Sky.g, Sky.b);
+  
+  // Draw Mr. Furious as a coloured circle
+  push();
+  noStroke();
+  fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+  ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+  pop();
+  //Make mrFurious slowly turn red
+  mrFurious.fill.g = mrFurious.fill.g -1
+  mrFurious.fill.b = mrFurious.fill.b -1
+  mrFurious.fill.g = constrain(mrFurious.fill.g,50,255)
+  console.log(mrFurious.fill.g)
 
-    fill(bird.r,bird.g,bird.b)
-    ellipse(bird.birdX,bird.birdY,bird.birdSize,bird.birdsize)
-
-    bird.birdX = bird.birdX +1
-
-    /*fill(random (0, 255),random(100, 255),0)
-    ellipse(mouseX, mouseY, ellipse_size, ellipse_size)
-
-    fill(random (0, 255),0,random(100, 255))
-    ellipse(mouseX/0.75,mouseY, ellipse_size, ellipse_size)
-
-    fill(0,random (0, 255),random(100, 255))
-    ellipse(mouseX * 0.5,mouseY, ellipse_size, ellipse_size)
-
-    fill(0,200,200)
-    rect(rect_size,height/2,rect_size, rect_size)
-
-    ellipse_size = ellipse_size-random(100,-100)
-    rect_size = rect_size+2
-    */
-
+  //Change Sky color to Night
+  Sky.r = Sky.r -1
+  Sky.b = Sky.b -1
+  Sky.g = Sky.g -1
+  //Constrain Sky color to a specific Dark Blue
+  Sky.b = constrain(Sky.b, 90, 255)
+  Sky.g = constrain(Sky.g,5,255)
+  Sky.r = constrain(Sky.r,11,255)
 }
