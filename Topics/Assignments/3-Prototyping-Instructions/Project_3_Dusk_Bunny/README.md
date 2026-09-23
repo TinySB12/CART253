962a02e99e1b004b97120c914fbd31ddda42e8c8
TinySB12 <saba.b.moghaddam@gmail.com>
2026-09-23 15:52:55 -0400

    Gave title to Project Dusk Bunny. Completed setup on Project Dusk Bunny as well.

--- Topics/Assignments/3-Prototyping-Instructions/Project_3_Dusk_Bunny/README.md
+++ Topics/Assignments/3-Prototyping-Instructions/Project_3_Dusk_Bunny/README.md
@@ -1,12 +1,12 @@
-# TITLE OF PROJECT
+# Dusk Bunny
 
-AUTHOR NAME
+Saba
 
 [View this project online](URL_FOR_THE_RUNNING_PROJECT)
 
 ## Description
 
-This description should help the reader understand what the program is, anything they should know to be able to experience it (controls, special features, etc.), and what the desired user experience is. For example:
+With this one, I'm going to attempt Color Interpolation for my background.
 
 ## Attribution
 
--- Topics/Assignments/3-Prototyping-Instructions/Project_3_Dusk_Bunny/js/script.js
+++ Topics/Assignments/3-Prototyping-Instructions/Project_3_Dusk_Bunny/js/script.js
@@ -12,12 +12,32 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    createCanvas(1000, 600);
+    colorMode(RGB);
+    noStroke();
 
+ //I'm going to ask what Describe does later. I'm making this setup based on a reference from the p5.js website.
+
+ let colorA = color(30, 0, 51);
+
+ let colorB = color(207, 76, 100);
+
+ let stripeCount= 9;
+
+ let stripeHeight= height / stripeCount;
+
+ for (let y = 0; y < height; y += stripeHeight) {
+    let fadeAmount = y / height;
+    let betweenColor = lerpColor(colorA, colorB, fadeAmount);
+
+    fill(betweenColor);
+    rect(0, y, width, stripeHeight);
+  }
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * I'll be drawing a silhouette of a rabbit here later!
 */
 function draw() {
 
